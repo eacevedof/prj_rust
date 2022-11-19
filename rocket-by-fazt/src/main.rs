3ac2224fa0cfcafc1946ac81febff9dcf885d61b
ioedumac <eacevedof@yahoo.es>
2022-11-19 14:46:02 +0100

    fazt ya func

--- rocket-by-fazt/src/main.rs
+++ rocket-by-fazt/src/main.rs
@@ -1,5 +1,17 @@
-#[macro_use] extern crate rocket
+//esto trae todos los módulos disponibles 
+#[macro_use] extern crate rocket;
 
-fn main() {
-    println!("Hello, world!");
+//otra forma particular es importar uno a uno
+//use rocket::routes
+
+#[get("/")]
+//función q devueve un string
+fn index() -> &'static str {
+    "hello world!"
+}
+
+#[launch]
+//funcion q no devuelve nada
+fn rocket() -> _{
+    rocket::build().mount("/", routes![index])
 }
