6e526310694175f070b3ba3fde07fd8523a99b36
ioedumac <eacevedof@yahoo.es>
2022-11-19 13:05:05 +0100

    rust leccion 2 ok

--- curso-luis-serrano-yt/lec-2/src/main.rs
+++ curso-luis-serrano-yt/lec-2/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-	let mivar_inmutable = 3;
+	let mut mivar_inmutable = 3;
 	println!("mi var inmutable es: {}!",mivar_inmutable);
+
+	mivar_inmutable = 7;
+	println!("mi var inmutable cambiada  es: {}!",mivar_inmutable);
+	
+	//debe ir en mayusculas
+	const MI_CONSTANTE: f32 = 3.1416;
+	println!("mi constante  es: {}!", MI_CONSTANTE );
+
 }
