011e68b4f5ad1e2e553cb40b53105a0b32415f18
ioedumac <eacevedof@yahoo.es>
2022-11-27 20:49:50 +0100

    ejemplo tipos funcs

--- udemy-curso-completo-del-lenguaje-rust/seccion-6/src/main.rs
+++ udemy-curso-completo-del-lenguaje-rust/seccion-6/src/main.rs
@@ -1,56 +1,33 @@
 fn main() {
-    println!("eduardoaf.com - variables en rust");
-    //enteros
-    let entero: i8 = 23;
-    let entero2: u8 = 40;
-    let entero3: i8 = -40;
+    mostrar_bienvenida();
+    seleccion_numero(8);
+    let nro = get_numero(28);
+    println!("El numero final es {}", nro);
+    
+    let exp_statemnt = {
+        10
+    };
+    println!("El numero final dos es: {}", exp_statemnt);
+    // para evitar convertirlo al vector String usando to_string()
+    // se puede definir la variable nombre como &str (string por referencia)
+    //saludar_con_nombre("Edualdo".to_string());
+    saludar_con_nombre("Edualdo");
 
-    //enteros en alfanumerico
-    let decimal= 98_222;
-    let hex = 0xff;
-    let octal = 0o77;
-    let binary = 0b1111_0000;
+}
 
-    //punto flotante
-    let float1 = 6.0;
-    let float32: f32 = 12.432;
+//fn saludar_con_nombre(nombre: String) {
+fn saludar_con_nombre(nombre: &str) {
+    println!("hola {}",nombre);
+}
 
-    //booleano
-    let verdadero = true;
-    let falso: bool = false;
+fn mostrar_bienvenida() {
+    println!("Bienvenidos a rust");
+}
 
-    //caracter con simple comilla
-    let caracter = 'a';
-    // https://emojipedia.org/
-    let emoji = "🇵🇪";
+fn seleccion_numero(nro: i32) {
+    println!("Tu numero es {}", nro);
+}
 
-    // Tipos compuestos:
-
-    //Tuplas
-    let tupla = ('h', 23, -45, 0.222);
-    let tupla2: (char, u64, i32, f64) = ('h', 23, -45, 0.222);
-
-    let (x, y, z, w) = tupla;
-
-    println!("el primer valor de la tupla {}", tupla.1);
-
-    //arreglos
-    let arreglo = [1, 2, 3, 4, 5];
-    print!("el segundo valor del arreglo es {}", arreglo[1]);
-
-    //[tipo; longitud]
-    let arreglo2: [i128; 5] = [1, 2, 3, 4, 5];
-
-    //strings, hay 2 tipos
-    let nombre = "Eduardoaf.com";
-    //string static es estatico pq se guarda en el binario final. Abarca el concepto de borrowing
-    //
-    let nombre2: &'static str = "eduardoaf.com";
-
-    //este tipo string es un array de unsigned a, permite que la memoria crezca y se aloja en la memoria heap
-    let apellido: String = "Julio".to_string();
-
-    let domicilio = String::new();
-    let domicilio = "mi casa".to_string();
-
+fn get_numero(nro: i32) -> i32 {
+    8
 }
