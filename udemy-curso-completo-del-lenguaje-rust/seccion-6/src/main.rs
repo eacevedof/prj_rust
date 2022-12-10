63baa189571d2abd38efa23c540ebbf9a548a24c
ioedumac <eacevedof@yahoo.es>
2022-12-10 20:36:38 +0100

    structs

--- udemy-curso-completo-del-lenguaje-rust/seccion-6/src/main.rs
+++ udemy-curso-completo-del-lenguaje-rust/seccion-6/src/main.rs
@@ -1,96 +1,25 @@
+//#[allow(dead_code)] //esto es para que no me alerte por codigo no usuado. ignoralo
+
 // https://learnxinyminutes.com/docs/es-es/rust-es/
-// use rand::random;
+// https://www.udemy.com/course/curso-completo-rust/learn/lecture/27199686#content
 
-struct Usuario {
-    nombre: String,
-    email: String,
-    nacimiento: i32,
-    activo: bool,
-    user_role: UserRole,
-    website: Website,
+/*
+//el Null de rust
+enum Option<T> {
+    Some(T),
+    None,
 }
-
-enum UserRole {
-    BASIC,
-    ADMIN
-}
-
-enum Website {
-    URL(String),
-    INSTAGRAM(String),
-    LINKEDIN(String),
-    FACEBOOK(String),
-}
+*/
 
 fn main() {
-    let number:u8 = rand::random();
-
-    let mut usuario =  Usuario {
-        nombre: "Eaf".to_string(), //una forma para vectorizar string
-        email: String::from("eaf@eduardoaf.com"), //otra forma para vectorizar string
-        nacimiento: 2000,
-        activo: true,
-        user_role: UserRole::BASIC,
-        website: Website::INSTAGRAM(String::from("@eaf")),
-    };
-
-    //incluir metodos en las structs
-    impl Usuario {
-        fn edad(&self) -> i32 {
-            let actual = 2022;
-            actual - self.nacimiento            
-        }
+    //let nombre: Option<String> = Some("eduardoaf.com".to_string());
+    let nombre: Option<String> = None;
+    
+    //esta estructura me permite controlar todas las opciones que pueda tener
+    //si es null hago algo si no otra cosa
+    match nombre {
+        None => println!("nombre is none"),
+        Some(nombre) => println!("El nombre es: {}", nombre),
     }
 
-    let edad = usuario.edad();
-    let role: UserRole = UserRole::BASIC;
-
-    println!(
-        "Usuario {} nacimiento {} email {} and random number is: {} y su edad es: {}", 
-        usuario.nombre, 
-        usuario.nacimiento, 
-        usuario.email,
-        number,
-        edad
-    );
-    usuario.activo = false;
-
-    //shorthand init
-    let usuario2: Usuario = get_nuevo_usuario(
-        String::from("Eduardo"), 
-        String::from("info@eduardoaf.com")
-    );
-
-    let usuario3: Usuario = Usuario {
-        nombre: "Juan".to_string(),
-        email: "otro@email.com".to_string(),
-        ..usuario2
-    };
-
-    let has_access = has_role_access(usuario.user_role);
-
-
-}
-
-fn go_to_website(website:Website) {
-    match website {
-        Website::INSTAGRAM => ;//algo
-        Website::FACEBOOK() => ;// abrelo en otra app
-    }
-}
-
-fn has_role_access(user_role:UserRole) -> bool {
-    match user_role {
-        UserRole::ADMIN => true,
-        UserRole::BASIC => false,
-    }
-}
-
-fn get_nuevo_usuario(nombre:String, email:String) -> Usuario {
-    return Usuario { 
-        nombre: nombre,
-        email: email,
-        nacimiento: 1980,
-        activo: true,
-    };
 }
